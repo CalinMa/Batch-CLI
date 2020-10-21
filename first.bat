efce600f3ce383c80e01a5adf8f807fd6d71e448
Calin Ma <63548302+CalinMa@users.noreply.github.com>
2020-10-21 23:48:12 +0300

    Add files via upload

---
+++ first.bat
@@ -0,0 +1,15 @@
+@echo off
+:: change the colors
+color 1c
+
+:: set up a title
+title MyFirstBatch
+
+:: print a message
+echo Your browser will open at www.google.com
+
+:: use "pause", so that the program stops and asks you to press any key
+pause
+start http://www.google.com
+
+
