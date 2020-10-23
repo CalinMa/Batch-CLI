a3a28e36e02a10cab51655534f2d839f7abc32e9
Calin Ma <63548302+CalinMa@users.noreply.github.com>
2020-10-23 16:23:05 +0300

    Add files via upload

---
+++ SpeakingBatch and Linkedinlink.bat
@@ -0,0 +1,18 @@
+@echo off
+:: change the colors
+color 1c
+
+:: set up a title
+title MyFirstBatch
+
+:: print a message
+echo Your browser will open at www.linkedin.com, and if you are here, hit connect!
+
+:: use "pause", so that the program stops and asks you to press any key
+pause
+start https://www.linkedin.com/in/calin-marincas-950080180/
+call "speakingbatch.bat"
+pause
+
+
+
